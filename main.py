8ffa36fd18a453e2fd05e06d2a7e8c780e19bade
Thirunavukarasu V Balaguru <141914777+btarasu1506@users.noreply.github.com>
2024-09-04 14:54:06 +0800

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.common.keys import Keys
 
-
+# DEMO TO ROS
 class MainTestCases():
     def __init__(self):
         self.driver = None
